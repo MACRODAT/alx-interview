45989740531c4da5892b2054648f1dd4fae53939
MACRODAT <macrodatservices@gmail.com>
2024-08-05 03:55:32 +0100

    Changes to the file

--- 0x01-lockboxes/0-lockboxes.py
+++ 0x01-lockboxes/0-lockboxes.py
@@ -22,7 +22,7 @@ def canUnlockAll(boxes):
     """
     n = len(boxes)
     seen_boxes = set([0])
-    unseen_boxes = set(boxes[0]).difference(set([0]))
+    unseen_boxes = set(boxes[0])
     while len(unseen_boxes) > 0:
         boxIdx = unseen_boxes.pop()
         if not boxIdx or boxIdx >= n or boxIdx < 0:
