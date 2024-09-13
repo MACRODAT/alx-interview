fecfab60860bed759b90dc52783ca81315ae0769
MACRODAT <macrodatservices@gmail.com>
2024-09-13 03:21:33 +0100

    Changes to the file

--- 0x07-rotate_2d_matrix/0-rotate_2d_matrix.py
+++ 0x07-rotate_2d_matrix/0-rotate_2d_matrix.py
@@ -1,10 +1,10 @@
 #!/usr/bin/python3
-"""2D matrix rotation module.
+"""rotate 3d matrix
 """
 
 
 def rotate_2d_matrix(matrix):
-    """Rotates an m by n 2D matrix in place.
+    """in place rotation
     """
     if type(matrix) != list:
         return
@@ -12,16 +12,16 @@ def rotate_2d_matrix(matrix):
         return
     if not all(map(lambda x: type(x) == list, matrix)):
         return
-    rows = len(matrix)
+    rws = len(matrix)
     cols = len(matrix[0])
     if not all(map(lambda x: len(x) == cols, matrix)):
         return
-    c, r = 0, rows - 1
-    for i in range(cols * rows):
-        if i % rows == 0:
+    c, r = 0, rws - 1
+    for i in range(cols * rws):
+        if i % rws == 0:
             matrix.append([])
         if r == -1:
-            r = rows - 1
+            r = rws - 1
             c += 1
         matrix[-1].append(matrix[r][c])
         if c == cols - 1 and r >= -1:
