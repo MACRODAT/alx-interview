bab4a27b94ffa6b7e4da87c6507ac99a4ad4cf2f
MACRODAT <macrodatservices@gmail.com>
2024-09-27 00:11:53 +0100

    Changes to the file

--- 0x09-island_perimeter/0-island_perimeter.py
+++ 0x09-island_perimeter/0-island_perimeter.py
@@ -1,15 +1,19 @@
 #!/usr/bin/python3
-
+""" Calculations:
 # my approach is simple:
 # 1. identify the squares surrounding the water
 # 2. remove the rest inland.
 # 3. calculate the adjacency of each to water
+"""
+
 
 def inGrid(_w, _h, i, j):
+    """Check if (x, y) is within the grid bounds."""
     return (i >= 0 and j >= 0 and i < _w and j < _h)
 
 
 def neighboors(i, j, _w, _h):
+    """Yield neighboring cell coordinates (excluding the center cell)."""
     combinations = [(i - 1, j), (i + 1, j), (i, j + 1), (i, j - 1)]
     for x, y in combinations:
         if inGrid(_w, _h, x, y):
@@ -17,6 +21,7 @@ def neighboors(i, j, _w, _h):
 
 
 def countWater(_w, _h, i, j, grid):
+    """Count the number of water cells surrounding a given land cell."""
     per = 0
     for x, y in neighboors(i, j, _w, _h):
         if grid[y][x] == 0:
@@ -25,6 +30,7 @@ def countWater(_w, _h, i, j, grid):
 
 
 def island_perimeter(grid):
+    """Calculate the perimeter of the island in the grid."""
     # let's do 1
     # @param grid the grid
     isIsland = False
