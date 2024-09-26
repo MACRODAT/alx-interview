e1f2fdcc41a2e56905b576a3dc182dc49baa961a
MACRODAT <macrodatservices@gmail.com>
2024-09-27 00:20:56 +0100

    Changes to the file

--- 0x09-island_perimeter/2.py
+++ 0x09-island_perimeter/2.py
@@ -1,12 +1,5 @@
 #!/usr/bin/python3
-""" Calculations:
-# my approach is simple:
-# 1. identify the squares surrounding the water
-# 2. remove the rest inland.
-# 3. calculate the adjacency of each to water
-"""
-
-
+"""Defines island perimeter finding function."""
 def inGrid(_w, _h, i, j):
     """Check if (x, y) is within the grid bounds."""
     return (i >= 0 and j >= 0 and i < _w and j < _h)
@@ -61,12 +54,38 @@ def island_perimeter(grid):
     return perimeter
 
 
+
+def island_perimeter2(grid):
+    """Return the perimiter of an island.
+    The grid represents water by 0 and land by 1.
+    Args:
+        grid (list): A list of list of integers representing an island.
+    Returns:
+        The perimeter of the island defined in grid.
+    """
+    width = len(grid[0])
+    height = len(grid)
+    edges = 0
+    size = 0
+
+    for i in range(height):
+        for j in range(width):
+            if grid[i][j] == 1:
+                size += 1
+                if (j > 0 and grid[i][j - 1] == 1):
+                    edges += 1
+                if (i > 0 and grid[i - 1][j] == 1):
+                    edges += 1
+    return size * 4 - edges * 2
+
+
 if __name__ == "__main__":
     grid = [
-        [0, 0, 0, 0, 0, 0],
-        [0, 1, 0, 0, 0, 0],
-        [0, 1, 0, 0, 0, 0],
-        [0, 1, 1, 1, 0, 0],
+        [0, 0, 1, 0, 0, 0],
+        [0, 0, 1, 0, 0, 0],
+        [0, 0, 1, 1, 0, 0],
+        [0, 0, 1, 1, 0, 0],
         [0, 0, 0, 0, 0, 0]
     ]
     print(island_perimeter(grid))
+    print(island_perimeter2(grid))
